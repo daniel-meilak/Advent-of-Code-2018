67dbbe5de28b45a9656359c954579d5ef98c98f3
Daniel <daniel.mlak@gmail.com>
2021-01-24 01:12:40 +0100

    Finished day 19

---
+++ week_3/day_19/day_19.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include<functional>
+#include"opcode.h"
+#include"../../Utils/utils.h"
+
+// forward function declarations
+int run_instructions(std::vector<int> reg, std::vector<std::vector<std::string>> input);
+
+int main(){
+
+    // read input into vector of vector of strings.
+    std::vector<std::string> delimiters = {" "};
+    std::vector<std::vector<std::string>> input = read_input_2D("input", delimiters);
+
+    // memory 
+    std::vector<int> reg(6,0);
+    
+    // part 1
+    std::cout << "Answer (part 1): " << run_instructions(reg,input) << std::endl;
+
+    // part 2
+    // From analyzing the opcode, find the sum of divisors of 10551340
+    int sum = 0;
+    int n   = 10551340;
+    for (int i=1; i<=n; i++){
+        if (n % i == 0){ sum += i; }
+    }
+    std::cout << "Answer (part 2): " << sum << std::endl;
+
+    return 0;
+}
+
+int run_instructions(std::vector<int> reg, std::vector<std::vector<std::string>> input){
+
+    // instruction pointer (iterator)
+    auto ip = reg.begin()+1;
+
+    // function reference
+    std::function<void(const int&, const int&, const int&,std::vector<int>&)> func;
+
+    while(*ip < (int)input.size()){
+        
+        // chose instruction pointed to by ip
+        std::vector<std::string> &line = input[*ip];
+
+        // optimization for part 2
+
+
+        // run approproate function
+        if      (line[0]=="addr"){ func = &addr; }
+        else if (line[0]=="addi"){ func = &addi; }
+        else if (line[0]=="mulr"){ func = &mulr; }
+        else if (line[0]=="muli"){ func = &muli; }
+        else if (line[0]=="banr"){ func = &banr; }
+        else if (line[0]=="bani"){ func = &bani; }
+        else if (line[0]=="borr"){ func = &borr; }
+        else if (line[0]=="bori"){ func = &bori; }
+        else if (line[0]=="setr"){ func = &setr; }
+        else if (line[0]=="seti"){ func = &seti; }
+        else if (line[0]=="gtir"){ func = &gtir; }
+        else if (line[0]=="gtri"){ func = &gtri; }
+        else if (line[0]=="gtrr"){ func = &gtrr; }
+        else if (line[0]=="eqir"){ func = &eqir; }
+        else if (line[0]=="eqri"){ func = &eqri; }
+        else if (line[0]=="eqrr"){ func = &eqrr; }
+        else if (line[0]=="#ip" ){
+            ip = reg.begin() + std::stoi(line[1]);
+            input.erase(std::remove(input.begin(), input.end(), line), input.end());
+            continue; 
+        }
+
+        // use function with arguments
+        func(std::stoi(line[1]), std::stoi(line[2]), std::stoi(line[3]), reg);
+
+        // increment value of ip
+        (*ip)++;
+    }
+
+    return reg[0];
+}
---
+++ week_3/day_19/opcode.h
@@ -0,0 +1,77 @@
+#ifndef OPCODE_H
+#define OPCODE_H
+
+#include<vector>
+#include<functional>
+
+void addr(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] + reg[b];
+}
+
+void addi(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] + b;
+}
+
+void mulr(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] * reg[b];
+}
+
+void muli(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] * b;
+}
+
+void banr(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] & reg[b];
+}
+
+void bani(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] & b;
+}
+
+void borr(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] | reg[b];
+}
+
+void bori(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a] | b;
+}
+
+void setr(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = reg[a];
+}
+
+void seti(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   reg[c] = a;
+}
+
+void gtir(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   if (a > reg[b]){ reg[c] = 1; }
+   else { reg[c] = 0; }
+}
+
+void gtri(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   if (reg[a] > b){ reg[c] = 1; }
+   else { reg[c] = 0; }
+}
+
+void gtrr(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   if (reg[a] > reg[b]){ reg[c] = 1; }
+   else { reg[c] = 0; }
+}
+
+void eqir(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   if (a == reg[b]){ reg[c] = 1; }
+   else { reg[c] = 0; }
+}
+
+void eqri(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   if (reg[a] == b){ reg[c] = 1; }
+   else { reg[c] = 0; }
+}
+
+void eqrr(const int &a, const int &b, const int &c, std::vector<int> &reg){
+   if (reg[a] == reg[b]){ reg[c] = 1; }
+   else { reg[c] = 0; }
+}
+
+#endif /* OPCODE_H */
