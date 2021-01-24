5fb530027cbc0f651f46eaa4d1689020992f5abf
Daniel <daniel.mlak@gmail.com>
2021-01-24 17:13:29 +0100

    FInished day 21

---
+++ week_3/day_21/day_21.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"opcode.h"
+#include"../../Utils/utils.h"
+
+int main(){
+
+    // read input into vector of vector of strings.
+    std::vector<std::string> delimiters = {" "};
+    std::vector<std::vector<std::string>> input = read_input_2D("input", delimiters);
+
+    std::vector<int> reg(6,0);
+
+    // manually reviewing the instructions
+
+    // part 1
+    // looking for first value in r[5] when ip==28
+
+    // part 2
+    // collect all numbers in r[5] when ip==28
+    // find last of these unique values (so last before repeat) 
+    
+    // run instructions has been modified to output these values and stop
+    run_instructions(reg,input);  
+
+    return 0;
+}
